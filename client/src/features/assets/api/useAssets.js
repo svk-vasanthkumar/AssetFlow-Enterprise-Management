0786636b8fbe405acccd5b651538b3ef02e6238d
satheshk61 <selvasathish986@gmail.com>
2026-07-12 11:37:58 +0530

    Update useAssets.js

--- client/src/features/assets/api/useAssets.js
+++ client/src/features/assets/api/useAssets.js
@@ -1,1 +1,13 @@
 
+import { useQuery } from '@tanstack/react-query';
+import { api } from '../../../services/api'; // Your Axios instance
+
+export const useAssets = () => {
+  return useQuery({
+    queryKey: ['assets'],
+    queryFn: async () => {
+      const response = await api.get('/assets');
+      return response.data;
+    },
+  });
+};
